f51997de96f553d18bce4a1dc19e9ec5748dc819
Manuel Hernández <manherna@ucm.es>
2018-08-18 13:10:32 +0200

    Profile hecho.

--- src/main/resources/static/js/pachangit.js
+++ src/main/resources/static/js/pachangit.js
@@ -1,10 +1,10 @@
 $(document).ready(function()
 {
   $(".pachanga").mouseenter(function(){
-      $(this).css({"background-color": "grey", "color": "white"});
+      $(this).css({"background-color": "#f9e79f", "color": "black"});
   });
   $(".pachanga").mouseleave(function(){
-      $(this).css({"background-color": "lightgrey", "color": "black"});
+      $(this).css({"background-color": "#f0f5c5", "color": "black"});
   });
 
 
